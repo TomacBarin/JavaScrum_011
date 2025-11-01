4dba3e27dc29ca7bf627cf18123916cc22261e48
Tomac Barin Jansson <tomacbarin@me.com>
2025-11-01 10:25:53 +0100

    First steps finished.

---
+++ script.js
@@ -0,0 +1,18 @@
+// Changing headline
+const h1Html = document.querySelector(".heado");
+h1Html.textContent = "This is a JavaScript Headline";
+
+// Adding a list
+
+const inputHtml = document.querySelector("#userInput");
+const buttonHtml = document.querySelector("#btn");
+const listHtml = document.querySelector("#todoList");
+
+inputHtml.addEventListener("keydown", (e) => {
+    if (e.key === "Enter" && inputHtml.value.trim() !== "") {
+        const li = document.createElement("li");
+        li.textContent = inputHtml.value;
+        listHtml.appendChild(li);
+        inputHtml.value = "";
+    }
+})
